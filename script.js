6a055899336dfb4943b07306c975c20316ef1a05
Trippin Sage <92277801+trippinsage@users.noreply.github.com>
2024-12-18 08:47:49 -0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const followButton = document.querySelector('button');
-    if (followButton) {
-        followButton.addEventListener('click', () => {
-            alert('Thanks for your interest! Please follow on the platforms listed.');
-        });
-    }
-});
